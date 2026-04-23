88aeaf97f1dc6aa617693dbe32f0e8ac9f5c5025
Robertus03 <pintearobert21@gmail.com>
2026-04-23 09:37:39 +0300

    Update git_task.py

--- git_task.py
+++ git_task.py
@@ -5,7 +5,7 @@ def f():
 s=pygame.display.set_mode((500,500));pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)");data=f();r=True
 while r:
  s.fill((0,0,0))
- for y in range(10):
+ for y in range(20):
   for x in range(10):pygame.draw.rect(s,data[y][x],(x*50,y*50,50,50))
  pygame.display.flip()
  for e in pygame.event.get():
